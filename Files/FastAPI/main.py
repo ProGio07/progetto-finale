ff2371005088affc4dee72406f8ad46c1cc7dd7c
ProGio07 <pratesigiordano13@gmail.com>
2024-05-20 22:50:18 +0200

    3.0
    added requirements.txt

--- Files/FastAPI/main.py
+++ Files/FastAPI/main.py
@@ -7,7 +7,6 @@ from models import *
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from typing import List
 
 
 # config fastAPI
